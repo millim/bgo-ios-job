628972445dd89c3f2503d5f95709af488f07ea42
millim <oblivion.millim@gmail.com>
2023-03-05 20:03:59 +0800

    add now

--- teams/daily.py
+++ teams/daily.py
@@ -4,6 +4,26 @@ __author__ = "sansui233"
 from lib.actions import op
 
 
+def now(left_round=0):
+    if left_round < 0:
+        return
+    op.chooseFriend([1000, 600]) 
+    wait(Template(r"common/攻击.jpeg", rgb=True), timeout=20, interval=1)
+    op.skillChoose(1, 1)
+    op.skillChoose(2, 3)
+    op.skillChoose(3, 1, 1)
+    op.attack(1, 1, 2, 3, 2, 2, delay=12)
+    wait(Template(r"common/攻击.jpeg", rgb=True), timeout=20, interval=1)
+    op.skillChoose(1, 2)
+    op.skillChoose(2, 1, 1)
+    op.skillChoose(2, 2)
+    op.attack(1, 1, 2, 3, 2, 2, delay=12)
+    wait(Template(r"common/攻击.jpeg", rgb=True), timeout=20, interval=1)
+    op.skillChoose(1, 3)
+    op.skillChoose(3, 3, 1)
+    op.attack(1, 1, 2, 3, 2, 2, delay=12)
+    op.ending(left_round > 0)
+
 # money 耍钱
 def money(left_round=0):
     if left_round < 0:
